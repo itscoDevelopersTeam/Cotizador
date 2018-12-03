502c4b8db8d262cb5ff8996b0038283179264c03
Pedro Farias <pedro.farias.sosa@gmail.com>
2018-12-03 00:35:18 -0600

    Update index.php
    no mames no puedo editar nada

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="#">Web Code</a>
+        <h1 class="navbar-brand" href="#">Web Code</h1>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
